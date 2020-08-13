6b5ead7966602d0d8d5ff1b2cb5a5215d03be3e9
pengoof <54857860+calvinwlim@users.noreply.github.com>
2020-08-13 11:52:27 -0700

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 function sendMessage() {
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discordapp.com/api/webhooks/723377304855642122/_8nywNO8eb6pvLZOoplDH-TL50HyKIR_2N863IITobAuRsbA0xVQu9DwWG2o7MxtHbbs");
+    request.open("POST", "";
 
     request.setRequestHeader('Content-type', 'application/json');
 
@@ -15,7 +15,7 @@ function sendMessage() {
         color: ("#62F1FF"),
         footer: {
             text: 'Pengoof#3333',
-            icon_url:'https://media.discordapp.net/attachments/705489083341733938/723402044135833661/image0.png?width=568&height=515'
+            icon_url:''
         }
       }
       
